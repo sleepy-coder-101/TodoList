699e2fca5ee6dea1536e5966d0e4a2c5b3777607
Monirul <monirul.work.1729@gmail.com>
2022-12-26 19:46:50 +0530

    Made changes to PORT

--- app.js
+++ app.js
@@ -149,6 +149,8 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
